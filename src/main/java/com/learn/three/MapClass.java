9ccbee170f48888b45ce667fc332e19b07153da0
Simson M <sims91502@gmail.com>
2024-05-08 07:50:37 +0530

    feat: complex and enum

--- src/main/java/com/learn/three/MapClass.java
+++ src/main/java/com/learn/three/MapClass.java
@@ -2,16 +2,22 @@ package com.learn.three;
 
 import com.learn.grp.three.Car;
 import com.learn.grp.three.Dealer;
+import com.learn.grp.three.Style;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MapClass {
     private static final Logger  log = LoggerFactory.getLogger(MapClass.class);
     public static void main(String[] args) {
-        Car car = Car.newBuilder().setCarId(1).setMake("tata").setModel("m2").build();
-        Car carTwo = Car.newBuilder().setCarId(2).setMake("tata").setModel("m3").build();
-        Car carThree = Car.newBuilder().setCarId(3).setMake("tata").setModel("m4").build();
-        Dealer dealer = Dealer.newBuilder().putDealer(1,carThree).build();
+//        Car car = Car.newBuilder().setCarId(1).setMake("tata").setModel("m2").build();
+        Car carTwo = Car.newBuilder()
+                .setCarId(2)
+                .setMake("tata")
+                .setStyle(Style.COUPE)
+                .setModel("m3")
+                .build();
+        Car carThree = Car.newBuilder().setCarId(3).setMake("tata").setModel("m4").setStyle(Style.sedan).build();
+        Dealer dealer = Dealer.newBuilder().putDealer(1,carThree).putDealer(2,carTwo).build();
         log.info("Dealer: {}",dealer);
         log.info("Dealer contains: {}",dealer.containsDealer(1));
         log.info("Dealer Map: {}",dealer.getDealerMap());
